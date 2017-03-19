7fea7b7e00298082a7a504208e5762c6b734214c
Sean <gilmore.seven@gmail.com>
2017-03-19 18:06:22 +1100

    add parent child relationship to wiki pages + documentation

--- app/models/wiki_page.rb
+++ app/models/wiki_page.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+# WikiPage resource
 class WikiPage < ApplicationRecord
+  has_many :sub_pages, class_name: 'WikiPage', foreign_key: 'parent_id', :dependent => :destroy
+  belongs_to :parent_page, class_name: 'WikiPage'
 end
