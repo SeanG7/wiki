52f3480c47e20cd28c197da5c5386b307e11ed0c
Sean <gilmore.seven@gmail.com>
2017-03-21 21:25:26 +1100

    add new columns to page table

--- db/migrate/20170319080436_create_pages.rb
+++ db/migrate/20170319080436_create_pages.rb
@@ -1,6 +1,11 @@
 class CreatePages < ActiveRecord::Migration[5.0]
   def change
     create_table :pages do |t|
+      t.string :title
+      t.text :content_md
+      t.text :content_html
+      t.string :author
+      t.references :parent
 
       t.timestamps
     end
